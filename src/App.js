795f7cd5b0e485166ca7803fc70c44e68a7f19d6
TienPhuc02 <dtp2352002@gmail.com>
2023-07-13 20:19:29 +0700

    UI sign up part1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./Contexts/auth-context";
+import SignUpPage from "./pages/Sign Up/SignUpPage";
 
 const App = () => {
   return (
     <div>
       <AuthProvider>
         <Routes>
-          <Route path="/"></Route>
+          <Route path="/sign-up" element={<SignUpPage />}></Route>
         </Routes>
       </AuthProvider>
     </div>
